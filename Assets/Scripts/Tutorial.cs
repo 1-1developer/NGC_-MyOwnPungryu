036d713b9f486fd643e34a38cfdb6d1d28890a0e
HooJungKim <sdhh1735@gmail.com>
2023-11-25 17:22:48 +0900

    [ADD] Tutotial Anim

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,12 +1,23 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
-{    
-    private void OnTriggerEnter2D(Collider2D collision)
+{
+    Image image;
+    [SerializeField] GameObject touchIcon;
+    [SerializeField] Vector2 touchIconAnimPosition;
+
+    private void Start()
     {
-        Debug.Log("Tutorial");
+        image = GetComponent<Image>();
+        image.DOFade(1.1f, 0.8f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        touchIcon.GetComponent<RectTransform>().DOAnchorPos(touchIconAnimPosition, 2.0f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    { 
         gameObject.SetActive(false);
     }
 
